76dc9dc2edce726097548302565a7e57cd071b7d
vs <vskrachkov@gmail.com>
2018-03-20 23:39:13 +0200

    refactor event view

--- sse/utils.py
+++ sse/utils.py
@@ -1,5 +1,36 @@
-def create_event(eventname, evntid, data):
-    return 'event: {eventname}\n' \
-           'id: {evntid}\n' \
-           'data: {data}\n\n'.format(
-        eventname=eventname, evntid=evntid, data=data)
+import json
+import time
+
+import datetime
+import uuid
+
+test_pipe = [item for item in range(0, 10)]
+
+def get_next_message():
+    try:
+        return test_pipe.pop()
+    except IndexError:
+        return
+
+
+def create_event(event_name, event_id, data):
+    return f'event: {event_name}\n' \
+           f'id: {event_id}\n' \
+           f'data: {data}\n\n'
+
+
+def get_event(last_event_id=None):
+    while 1:
+        print(last_event_id)
+        time.sleep(1)
+        next_msg = get_next_message()
+        if next_msg:
+            data = {
+                'message': next_msg,
+                'time': datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+            }
+            yield create_event(
+                event_name='test',
+                event_id=uuid.uuid4(),
+                data=json.dumps(data)
+            )
--- sse/views.py
+++ sse/views.py
@@ -1,24 +1,14 @@
-import time
-import datetime
-import json
-
-from django.http import HttpResponse
+from django.http import StreamingHttpResponse
 from django.shortcuts import render
 
-from sse.utils import create_event
+from .utils import get_event
 
 
 def index(request):
     return render(request, template_name='sse/index.html')
 
 def events(request):
-    print('last event id: ', request.META.get('HTTP_LAST_EVENT_ID'))
-    data = {
-        'some': 'data',
-        'another': 'data',
-        'time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    }
-    str_data = json.dumps(data)
-    _id = time.time()
-    content = create_event('test', _id, str_data)
-    return HttpResponse(content=content, content_type='text/event-stream')
+    last_event_id = request.META.get('HTTP_LAST_EVENT_ID')
+
+    return StreamingHttpResponse(streaming_content=get_event(last_event_id),
+                                 content_type='text/event-stream')
